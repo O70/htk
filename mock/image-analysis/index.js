a4bf8287ccd1695160e410d4b3cc2b7f53346520
鬼王 <THRAEX@aliyun.com>
2021-09-27 11:07:42 +0800

    feat(image/analysis): service

--- mock/image-analysis/index.js
+++ mock/image-analysis/index.js
@@ -5,14 +5,20 @@ module.exports = [
   {
     url: '/api/thraex/image/analysis',
     type: 'get',
-    response: _ => ({ code: 20000, data: new Array(0).fill('Item').map((it, i) => ({
-      // id: uuidv4(),
-      nation: `Nation-${i}`,
-      type: ['岩心或岩屑', '野外露头剖面'][i % 2],
-      location: `location-${i}`,
-      era: `era-${i}`,
-      stratum: `stratum-${i}`
-    })) })
+    // response: _ => ({ code: 20000, data: service.list() })
+    response: async _ => {
+      const data = await service.list()
+      console.debug(data)
+      return { code: 20000, data }
+    }
+    // response: _ => ({ code: 20000, data: new Array(0).fill('Item').map((it, i) => ({
+    //   // id: uuidv4(),
+    //   nation: `Nation-${i}`,
+    //   type: ['岩心或岩屑', '野外露头剖面'][i % 2],
+    //   location: `location-${i}`,
+    //   era: `era-${i}`,
+    //   stratum: `stratum-${i}`
+    // })) })
   },
   {
     url: '/api/thraex/image/analysis',
--- mock/image-analysis/service.js
+++ mock/image-analysis/service.js
@@ -1,15 +1,40 @@
-const fs = require('fs/promises')
+const fsp = require('fs/promises')
+
 const { v4: uuidv4 } = require('uuid')
+
+const [DB_DIR, DB_PATH] = ['./db.tmp', './db.tmp/db.json']
 
 class Service {
   constructor() {
-    console.debug('constructor...')
-    fs.mkdir('./db')
-    fs.writeFile('gui')
+    fsp.access(DB_DIR)
+      .then(_ => fsp.access(DB_PATH).catch(_ => this.writeFile()))
+      .catch(_ => fsp.mkdir(DB_DIR).finally(_ => this.writeFile()))
+  }
+
+  writeFile(data = []) {
+    fsp.writeFile(DB_PATH, JSON.stringify(data, null, '\t'))
+  }
+
+  async list() {
+    // fsp.readFile(DB_PATH).then(content => {
+    //   const rows = JSON.parse(content.toString())
+    //   console.debug(rows)
+    // })
+    const content = await fsp.readFile(DB_PATH)
+    const rows = JSON.parse(content.toString())
+    // console.debug(rows)
+    return rows
   }
 
   save(data) {
-    console.debug(data)
+    Object.assign(data, { id: uuidv4() })
+    fsp.readFile(DB_PATH).then(content => {
+      console.debug(content)
+      const rows = JSON.parse(content.toString())
+      console.debug(rows)
+      this.writeFile([data, ...rows])
+    })
+
     return Object.assign(data, { id: uuidv4() })
   }
 }
